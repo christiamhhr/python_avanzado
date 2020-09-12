906bee3734bd1464c8cb696886cefdcb1fdb77e5
chriro <cristian_111_2@gmail.com>
2020-09-12 11:11:59 -0500

    operaciones con sets, union interseccion

---
+++ operacines_sets.py
@@ -0,0 +1,12 @@
+a ={1,2}
+
+a.discard(1)
+a.add(3)  # de uno en uno
+print(a)
+a.update({5,6})  #de a varios
+print(a)
+set1 = {1,1,2,3,4,4}.union({1,2,3,4,5,6,6,6})  #tambien se puede usar intersection
+print(set1)
+
+
+
